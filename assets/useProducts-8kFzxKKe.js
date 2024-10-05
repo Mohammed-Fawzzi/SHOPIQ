b0359e58a53d6216be87b8ef7aec4b80bafee905
Mohammed Fawzzi <mohamedfawzzimohamed@gmail.com>
2024-10-05 16:07:18 +0300

    Updates

---
+++ assets/useProducts-8kFzxKKe.js
@@ -0,0 +1,1 @@
+import{a as r}from"./axios-C8DqakIB.js";import"./index-BAlKlOWj.js";import{u as s}from"./useQuery-USuf5USd.js";function i(){return r.get("https://ecommerce.routemisr.com/api/v1/products?sort=-price")}function m(t){return r.get(`https://ecommerce.routemisr.com/api/v1/products/${t}`)}function p(t,e){return s(t,e,{select:o=>o.data.data})}export{m as a,i as g,p as u};
